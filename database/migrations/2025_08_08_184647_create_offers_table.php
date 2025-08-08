c40b5a810f143fba2a4cc8d1986bf8a9f3e606a1
delwarhossaindev <delwarhossain1103104@gmail.com>
2025-08-09 01:28:32 +0600

    first commit

---
+++ database/migrations/2025_08_08_184647_create_offers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('code')->unique();
+            $table->decimal('discount_percent', 5, 2)->nullable();
+            $table->decimal('discount_amount', 10, 2)->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
